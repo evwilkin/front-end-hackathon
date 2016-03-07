9b047aadf90ef8f78671027904b63e0102e25b19
evwilkin <evan.wilkinson@gmail.com>
2016-03-07 01:58:10 -0800

    added all images, video on home, and added image transparency and text on each location page

--- js/script.js
+++ js/script.js
@@ -3,7 +3,10 @@ $(document).ready(function() {
     anchors: ['home', 'florence', 'venice', 'santorini', 'athens', 'reykjavik', 'iceland', 'end'],
     navigation: true,
     navigationTooltips: ['home', 'florence', 'venice', 'santorini', 'athens', 'reykjavik', 'iceland', 'end'],
-    fixedElements: "#navbar",
-    paddingTop: '80px'
+    loopBottom: true,
+/*    fixedElements: 'nav',
+    paddingTop: '50px'];*/
+/*    fixedElements: ["nav#header", "nav.row", "div.columns", "ul", "li", "a"]*/
+    // paddingTop: '80px'
   });
 });
