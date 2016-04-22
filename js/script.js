2dec8b1c359633cd05127cd1711d338d6a1cab1f
evwilkin <evan.wilkinson@gmail.com>
2016-04-22 14:18:50 -0700

    added about page

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 $(document).ready(function() {
   $('#fullpage').fullpage({
-    anchors: ['home', 'florence', 'venice', 'santorini', 'athens', 'reykjavik', 'iceland', 'end'],
+    anchors: ['home', 'florence', 'venice', 'santorini', 'athens', 'reykjavik', 'iceland', 'about'],
     navigation: true,
-    navigationTooltips: ['home', 'florence', 'venice', 'santorini', 'athens', 'reykjavik', 'iceland', 'end'],
-    loopBottom: true
+    navigationTooltips: ['home', 'florence', 'venice', 'santorini', 'athens', 'reykjavik', 'iceland', 'about'],
+    loopBottom: false,
+    afterRender: function(){
+      scrollOverflow: true
+    }
   });
 
   $('#showMenu').on("click", function() {
-    $('#menu').slideToggle();
+    $('#menu').slideToggle(350);
   });
 
   $('a.button').on("click", function() {
-    $('#menu').slideToggle();
+    $('#menu').slideToggle(350);
   });
 });
